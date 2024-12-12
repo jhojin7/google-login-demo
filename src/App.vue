c885700ac0d3e4dfe76f876c22a309b197852be3
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2024-12-13 02:19:11 +0900

    Add Google Sign-In integration and update dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,92 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div id="g_id_onload" :data-client_id="googleClientId" data-callback="handleCredentialResponse">
+    </div>
+    <div class="g_id_signin" data-type="standard"></div>
+    <div v-if="accessToken">
+      <p>
+        Access token:<br> <span style="word-break: break-all;">{{ accessToken }}</span>
+      </p>
+      <ul>
+        <p>{{ todos }}</p>
+        <!-- iterate over list of todos: type: list[str] -->
+        <li v-for="todo in todos" :key="todo">
+          {{ todo }}
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      No access token
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import { GOOGLE_CLIENT_ID } from './config';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  data() {
+    return {
+      accessToken: null,
+      googleClientId: GOOGLE_CLIENT_ID,
+      todos: [],
+    };
+  },
+  created() {
+    this.accessToken = localStorage.getItem('accessToken');
+    this.todos = [];
+    if (this.accessToken) {
+      this.fetchTodos();
+    }
+  },
+  mounted(){
+    window.handleCredentialResponse = this.handleCredentialResponse;
+    // const script = document.createElement('script');
+    // script.src = 'https://accounts.google.com/gsi/client';
+    // script.async = true;
+    // document.body.appendChild(script);
+  },
+  methods: {
+    handleCredentialResponse(response) {
+      console.log("Encoded JWT ID token: " + response.credential);
+      // Decode the JWT token to get user information
+      const userObject = JSON.parse(atob(response.credential.split('.')[1]));
+      console.log("User object:", userObject);
 
+      const mockGoogleResponse = {
+        email: userObject.email,
+        name: userObject.name,
+      };
+      console.log("mockGoogleResponse:", mockGoogleResponse);
+
+      axios.post('http://localhost:8000/login/google', mockGoogleResponse)
+        .then(response => {
+          console.log("Login response:", response.data);
+          this.accessToken = response.data.access_token;
+          this.fetchTodos();
+          localStorage.setItem('accessToken', response.data.access_token);
+        })
+        .catch(err => {
+          console.error("Error posting login data:", err);
+        });
+    },
+    async fetchTodos() {
+      const response = await axios.get('http://localhost:8000/todos', {
+        headers: { Authorization: `Bearer ${this.accessToken}` },
+      });
+      this.todos = response.data
+      console.log("Todos:", this.todos);
+    },
+  },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  align-items: center;
+  width: 70vh;
+  margin: 10vh auto;
 }
 </style>
